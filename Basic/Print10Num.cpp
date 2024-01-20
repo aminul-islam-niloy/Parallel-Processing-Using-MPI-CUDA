cdbf79ffbc22882d91c160cee46b29e6b7db1f0e
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-01-20 17:38:56 +0600

    Print 10 Num with for loop

---
+++ Basic/Print10Num.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Calculate the range of numbers to print for each process
+    int numbersPerProcess = 10 / size;
+    int startNumber = rank * numbersPerProcess + 1;
+    int endNumber = (rank + 1) * numbersPerProcess;
+
+    // Ensure the last process prints any remaining numbers
+    if (rank == size - 1) {
+        endNumber = 10;
+    }
+
+    // Print numbers for each process
+    for (int i = startNumber; i <= endNumber; ++i) {
+        std::cout << "Process " << rank << ": " << i << std::endl;
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}
+
+
+//Note: How to run
+// Build this project or file
+//mpiexec -n 4 ./LabTest
+//mpiexec -n numofProcess ./FileName
